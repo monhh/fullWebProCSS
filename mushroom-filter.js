c3cd567e3b92187f2a2c6a9ab52cd5f5779e1be4
mon.ptell <mon.medialab@gmail.com>
2025-04-18 13:03:45 +0200

    filtering cards

--- mushroom-filter.js
+++ mushroom-filter.js
@@ -22,10 +22,35 @@ seasonalFilter.addEventListener('change', updateFilter);
 edibleFilter.addEventListener('change', updateFilter);
 
 function updateFilter(e) {
-   // console.log(e.target.value);
+    // console.log(e.target.value);
     //console.log(e.target.name);
     const filterType = e.target.name;
     currentFilters[filterType] = e.target.value;
     //console.log(currentFilters);
+
+    filterCards();
 }
 
+
+function filterCards() {
+    cards.forEach((card) => {
+        //console.log(card);
+        const season = card.querySelector('[data-season]').dataset.season;
+        const edible = card.querySelector("[data-edible]").dataset.edible;
+        //console.log(season, edible);
+
+        const matchesSeason = currentFilters.season === season;
+        const matchesEdible = currentFilters.edible === edible;
+        //console.log(matchesSeason, matchesEdible);
+        
+        if (
+            (matchesEdible || currentFilters.edible === "all") && 
+            (matchesSeason || currentFilters.season === "all")
+        ) {
+            card.hidden = false;
+        } else {
+            card.hidden = true;
+        }
+    });
+}
+//filterCards();
