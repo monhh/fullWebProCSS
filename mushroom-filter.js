d3f95b8f123c055e400d2ffa39437c45708954f7
mon.ptell <mon.medialab@gmail.com>
2025-04-18 03:14:57 +0200

    updating filters

--- mushroom-filter.js
+++ mushroom-filter.js
@@ -1,23 +1,31 @@
 const cards = document.querySelectorAll('.mushroom-guide .card');
-const seasonedFiltert = document.querySelector('#season');
+const seasonalFilter = document.querySelector('#season');
 const edibleFilter = document.querySelector('#edible');
 
 const currentFilters = {
-    seasoned: "all",
+    season: "all",
     edible: "all"
 };
 
-seasonedFiltert.addEventListener('change', (event) => {
-    /* console.log(event); */
-    /* console.log(event.target.value); */
+/* seasonedFiltert.addEventListener('change', (event) => {
     currentFilters.seasoned = event.target.value;
     console.log(currentFilters);
 });
 
-
 seasonedFiltert.addEventListener("change", (event) => {
-    /* console.log(event); */
-    /* console.log(event.target.value); */
-    currentFilters.edible = event.target.value;
+    edibleFilterFilters.edible = event.target.value;
     console.log(currentFilters);
-});
+}); */
+
+
+seasonalFilter.addEventListener('change', updateFilter);
+edibleFilter.addEventListener('change', updateFilter);
+
+function updateFilter(e) {
+   // console.log(e.target.value);
+    //console.log(e.target.name);
+    const filterType = e.target.name;
+    currentFilters[filterType] = e.target.value;
+    //console.log(currentFilters);
+}
+
